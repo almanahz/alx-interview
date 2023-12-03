7219105e1fb47e7edc0a3664f303afe06fbead60
Abdulsalam <75430767+almanahz@users.noreply.github.com>
2023-12-03 09:48:37 +0100

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -10,9 +10,12 @@ def pascal_triangle(n):
     triangle = []
     if type(n) is not int or n <= 0:
         return triangle
-    for i in range(num_rows):
-        row = [1] * (i + 1)
-        for j in range(1, i):
-            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
-        triangle.append(row)
+    for i in range(n):
+        line = []
+        for j in range(i + 1):
+            if j == 0 or j == i:
+                line.append(1)
+            elif i > 0 and j > 0:
+                line.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        triangle.append(line)
     return triangle
